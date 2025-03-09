ef4ff0d8c6c939b7f1c652ef4718e8568c655288
jmaroju <Jagadheesh.Maroju@united.com>
2025-03-08 20:25:19 -0600

    add mvc project

---
+++ src/Project/Integration/code/Controllers/ReactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+
+namespace SitecoreMvc.Project.Integration.Controllers
+{
+    public class ReactController : Controller
+    {
+        public ActionResult RenderReactApp()
+        {
+
+            var dataSource = RenderingContext.Current.Rendering.Item;
+            var model = new
+            {
+                title = dataSource.Fields["Title"].Value,
+                resultsPerPage = dataSource.Fields["ResultsPerPage"].Value,
+            };
+            return View("~/Views/React/RenderReactApp.cshtml", model);
+        }
+    }
+}
---
+++ src/Project/Integration/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SitecoreMvc.Project.Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SitecoreMvc.Project.Integration")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f46f3b99-d598-4aa1-bf3e-896b640eb77b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
